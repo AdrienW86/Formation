f7fa5aecce800b03389f5c9db8b0c34eac010375
Codev <codev@codevs-air.home>
2026-04-21 23:27:25 +0200

    mise en ligne

--- components/BackgroundGenerator/Background.jsx
+++ components/BackgroundGenerator/Background.jsx
@@ -14,7 +14,13 @@ export default function Background() {
         sizes="100vw"
         className={`${styles.bannerImage} ${styles.mobile}`}
       />
-
+      <div className={styles.txt}>
+        <h1> Créez le <span className={styles.span}>CV <br/></span> qui vous démarque</h1>
+        <p className={styles.p}> 
+            Un CV professionnel en quelques minutes. <br></br>
+            Mettez toutes les chances de votre côté.
+        </p>
+      </div>
       {/* Desktop */}
       <Image
         src="/background-desktop.png"
